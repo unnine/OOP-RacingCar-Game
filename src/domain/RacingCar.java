58b0b70afcfef4f53ad2007f370e4d443bac23fc
unnine <eonninej@gmail.com>
2023-10-30 17:55:07 +0900

    change to record class RacingCar

--- src/domain/RacingCar.java
+++ src/domain/RacingCar.java
@@ -1,20 +1,9 @@
 package domain;
 
-public class RacingCar implements Car{
-
-    private final String name;
-
-    public RacingCar(String name) {
-        this.name = name;
-    }
+public record RacingCar(String name) implements Car {
 
     @Override
     public void go() {
         System.out.println(name + " 전진합니다.");
-    }
-
-    @Override
-    public String name() {
-        return name;
     }
 }
